0b0406d848398f02fe90e586c576823c4c5e1020
Ali Erkilic <erkilic.alican@gmail.com>
2017-10-13 22:33:46 +0300

    Storyboard guncellemeleri yapildi

---
+++ ASLI/SideMenuVC.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuVC.swift
+//  ASLI
+//
+//  Created by Ali Can Erkilic on 10/13/17.
+//  Copyright © 2017 Ali Can Erkilic. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuVC: UITableViewController {
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
